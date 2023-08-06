f2551c93a80dac0db2d72bde2e920a16486d88cf
shalzuth <shalzuth@users.noreply.github.com>
2023-08-05 22:06:34 -0700

    add battle cam mods

--- BG3Cam/MainWindow.cs
+++ BG3Cam/MainWindow.cs
@@ -10,15 +10,12 @@ namespace BG3Cam
             InitializeComponent();
         }
         nint obj;
-        int camOffset;
+        int battleCamOffset;
+        int worldCamOffset;
         int camMaxOffset;
         int camMaxAbsOffset;
         int camTiltOffset;
-        float defaultZoomMax = 12f;
-        float defaultZoomMin = 3.5f;
-        float defaultMaxAbs = 1f;
-        float defaultPitchMaxZoom = 1f;
-        float defaultPitchMinZoom = 1f;
+        Dictionary<nint, float> defaultVals = new Dictionary<nint, float>();
         int prevMouseY;
         float curTilt;
         void MainWindow_Load(object sender, EventArgs e)
@@ -36,8 +33,11 @@ namespace BG3Cam
 
             var camFuncBytes = mem.ReadProcessMemory(camFuncAddr, 0x100);
 
-            var camOffsetStart = mem.FindPattern("F6 C1 01 75 07 49 8D 80", bytes: camFuncBytes);
-            camOffset = BitConverter.ToInt32(camFuncBytes, (int)camOffsetStart + 8);
+            var battleCamOffsetStart = mem.FindPattern("49 8D 80 ? ? ? ? F6 C1 01", bytes: camFuncBytes);
+            battleCamOffset = BitConverter.ToInt32(camFuncBytes, (int)battleCamOffsetStart + 3);
+
+            var worldCamOffsetStart = mem.FindPattern("F6 C1 01 75 07 49 8D 80", bytes: camFuncBytes);
+            worldCamOffset = BitConverter.ToInt32(camFuncBytes, (int)worldCamOffsetStart + 8);
 
             var maxDistOffAddr = mem.FindPattern("C3 F3 0F 10 48", bytes: camFuncBytes);
             camMaxOffset = camFuncBytes[maxDistOffAddr + 5];
@@ -48,40 +48,50 @@ namespace BG3Cam
             var camTilt = mem.FindPattern("C3 F3 0F 10 80 ? ? ? ? F3 0F 10 88 ? ? ? ? 0F 14 C8 66 48 0F 7E C8 C3");
             camTiltOffset = mem.ReadProcessMemory<int>(camTilt + 5);
 
-            var curMaxZoom = mem.ReadProcessMemory<float>(obj + camOffset + camMaxOffset); // default val is 12, addr offset is 0x7b4 in release patch
-            if (curMaxZoom != defaultZoomMax)
+            var curMaxZoom = AddDefaultVal(obj + worldCamOffset + camMaxOffset); // default val is 12, addr offset is 0x7b4 in release patch
+            if (curMaxZoom == 12)
             {
                 MessageBox.Show("Max Zoom not found", "Bad Game State");
                 Environment.Exit(0);
             }
             maxZoomVal.Value = (decimal)100f;
             minZoomVal.Value = (decimal)0.5f;
-            defaultZoomMin = mem.ReadProcessMemory<float>(obj + camOffset + camMaxOffset + 4);
-            defaultMaxAbs = mem.ReadProcessMemory<float>(obj + camOffset + camMaxAbsOffset);
-            curTilt = defaultPitchMaxZoom = mem.ReadProcessMemory<float>(obj + camOffset + camTiltOffset);
-            defaultPitchMinZoom = mem.ReadProcessMemory<float>(obj + camOffset + camTiltOffset + 4);
-            mem.WriteProcessMemory(obj + camOffset + camMaxAbsOffset, 10000.0f);
-            mem.WriteProcessMemory(obj + camOffset + camTiltOffset, curTilt);
-            mem.WriteProcessMemory(obj + camOffset + camTiltOffset + 4, curTilt);
+            AddDefaultVal(obj + worldCamOffset + camMaxOffset + 4);
+            AddDefaultVal(obj + battleCamOffset + camMaxOffset);
+            AddDefaultVal(obj + battleCamOffset + camMaxOffset + 4);
+            curTilt = AddDefaultVal(obj + worldCamOffset + camTiltOffset);
+            AddDefaultVal(obj + worldCamOffset + camTiltOffset + 4);
+            AddDefaultVal(obj + worldCamOffset + camTiltOffset + 8);
+            AddDefaultVal(obj + worldCamOffset + camTiltOffset + 12);
+            AddDefaultVal(obj + battleCamOffset + camTiltOffset);
+            AddDefaultVal(obj + battleCamOffset + camTiltOffset + 4);
+            AddDefaultVal(obj + battleCamOffset + camTiltOffset + 8);
+            AddDefaultVal(obj + battleCamOffset + camTiltOffset + 12);
+            mem.WriteProcessMemory(obj + worldCamOffset + camMaxAbsOffset, 10000.0f);
+            mem.WriteProcessMemory(obj + worldCamOffset + camTiltOffset, curTilt);
+            mem.WriteProcessMemory(obj + worldCamOffset + camTiltOffset + 4, curTilt);
             ChangePitchOnMouseMove();
         }
         bool running = true;
+        float AddDefaultVal(nint addr)
+        {
+            defaultVals[addr] = mem.ReadProcessMemory<float>(addr);
+            return defaultVals[addr];
+        }
         void MainWindow_FormClosing(object sender, FormClosingEventArgs e)
         {
             running = false;
-            mem.WriteProcessMemory(obj + camOffset + camMaxOffset, defaultZoomMax);
-            mem.WriteProcessMemory(obj + camOffset + camMaxOffset + 4, defaultZoomMin);
-            mem.WriteProcessMemory(obj + camOffset + camMaxAbsOffset, defaultMaxAbs);
-            mem.WriteProcessMemory(obj + camOffset + camTiltOffset, defaultPitchMaxZoom);
-            mem.WriteProcessMemory(obj + camOffset + camTiltOffset + 4, defaultPitchMinZoom);
+            foreach(var val in defaultVals) mem.WriteProcessMemory(val.Key, val.Value);
         }
         void maxZoomVal_ValueChanged(object sender, EventArgs e)
         {
-            mem.WriteProcessMemory(obj + camOffset + camMaxOffset, (float)maxZoomVal.Value);
+            mem.WriteProcessMemory(obj + worldCamOffset + camMaxOffset, (float)maxZoomVal.Value);
+            mem.WriteProcessMemory(obj + battleCamOffset + camMaxOffset, (float)maxZoomVal.Value);
         }
         void minZoomVal_ValueChanged(object sender, EventArgs e)
         {
-            mem.WriteProcessMemory(obj + camOffset + camMaxOffset + 4, (float)minZoomVal.Value);
+            mem.WriteProcessMemory(obj + worldCamOffset + camMaxOffset + 4, (float)minZoomVal.Value);
+            mem.WriteProcessMemory(obj + battleCamOffset + camMaxOffset + 4, (float)minZoomVal.Value);
         }
         async Task ChangePitchOnMouseMove()
         {
@@ -94,8 +104,14 @@ namespace BG3Cam
                     if (diff != 0)
                     {
                         curTilt += diff * 0.05f;
-                        mem.WriteProcessMemory(obj + camOffset + camTiltOffset, curTilt);
-                        mem.WriteProcessMemory(obj + camOffset + camTiltOffset + 4, curTilt);
+                        mem.WriteProcessMemory(obj + worldCamOffset + camTiltOffset, curTilt);
+                        mem.WriteProcessMemory(obj + worldCamOffset + camTiltOffset + 4, curTilt);
+                        mem.WriteProcessMemory(obj + worldCamOffset + camTiltOffset + 8, curTilt);
+                        mem.WriteProcessMemory(obj + worldCamOffset + camTiltOffset + 12, curTilt);
+                        mem.WriteProcessMemory(obj + battleCamOffset + camTiltOffset, curTilt);
+                        mem.WriteProcessMemory(obj + battleCamOffset + camTiltOffset + 4, curTilt);
+                        mem.WriteProcessMemory(obj + battleCamOffset + camTiltOffset + 8, curTilt);
+                        mem.WriteProcessMemory(obj + battleCamOffset + camTiltOffset + 12, curTilt);
                         prevMouseY = Cursor.Position.Y;
                     }
                 }
